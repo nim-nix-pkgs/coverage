0a01b7002a0f0c6b9d3d8a33730b197c3bdff830
hugosenari <hugosenari@gmail.com>
2022-03-22 01:08:16 -0300

    chore: re-index ../nimpkgs/c/coverage

--- master/flake.nix
+++ master/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-coverage-master.flake = false;
+  inputs.src-coverage-master.ref   = "refs/heads/master";
   inputs.src-coverage-master.owner = "yglukhov";
-  inputs.src-coverage-master.ref   = "master";
   inputs.src-coverage-master.repo  = "coverage";
   inputs.src-coverage-master.type  = "github";
   
