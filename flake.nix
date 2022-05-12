fa28d4c41c33ed8e4ebdee9cb92ad7690824d273
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:44 -0300

    chore: re index coverage

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."coverage-master".dir   = "master";
+  inputs."coverage-master".owner = "nim-nix-pkgs";
+  inputs."coverage-master".ref   = "master";
+  inputs."coverage-master".repo  = "coverage";
+  inputs."coverage-master".type  = "github";
+  inputs."coverage-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."coverage-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
